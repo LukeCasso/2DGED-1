84c89bea1e3db1e2340092731b6a40828d2fee35
James Farrell <d00206780@student.dkit.ie>
2021-10-05 11:04:13 +0100

    Add Ball Bounds Collision, Add Left Paddle

--- Games/Pong/js/Vector2.js
+++ Games/Pong/js/Vector2.js
@@ -16,10 +16,12 @@ class Vector2 {
     }
     
     set x(value) {
-        this._x = (value > 0) ? value : 0;
+        // this._x = (value > 0) ? value : 0;
+        this._x = value;
     }
     set y(value) {
-        this._y = (value > 0) ? value : 0;
+        // this._y = (value > 0) ? value : 0;
+        this._y = value;
     }
 
     normalize() {
--- Games/Pong/js/pong.js
+++ Games/Pong/js/pong.js
@@ -22,71 +22,37 @@ function loadGame() {
 // Set up game values
 function initializeGame() {
     // Create a ball at the center of our screen
-    ball = new Arc(new Vector2(canvas.clientWidth / 2, canvas.clientHeight / 2), 10, 0, Math.PI * 2);
-    // Create a vector which will determine where the ball will 
-    // move at the start of the game
+    ball = new Arc(
+        new Vector2(canvas.clientWidth / 2, canvas.clientHeight / 2), 
+        10, 
+        0, 
+        Math.PI * 2
+    );
 
-    // To do this, we will use JavaScripts built-in Math.random() function
-
-    // What does random do?
-    // Random creates a random number between 0 and 1 - for example, this number could be 0.2
-
-    // What does multiplying it by 10 do?
-    // Multiplying it by 10 turns 0.2 into 2
-    // So, effectively, we are now creating a random number between 0 and 10
-
-    // What does subtracting 5 do?
-    // Subtracting 5 turns our 2 into -3
-    // So, effectively, we are now creating a random number between -5 and 5
-
-    let randomX = Math.floor((Math.random() * 10) - 5);
-    let randomY = Math.floor((Math.random() * 10) - 5);
+    let randomX = getRandomInRangeExcluding(-10, 10, 0);
+    let randomY = getRandomInRangeExcluding(-0.5, 0.5, 0);
 
     ballMovementVector = new Vector2(randomX, randomY);
 
-    // Okay, but what's wrong with this?
-    // There are a number of things
-    // One, the ball sometimes doesn't move - this is because both the x
-    // and the y value that were generated were equal to 0
-
-    // Also, sometimes the ball moves directly up or directly down - this is because
-    // the x value is equal to 0, and the y value is not
-
-    // Finally, we can see the speed of the ball changes each time we start our game
-
-    // So, how do we fix these problems?
-    // Well first of all, we will need to create a function that will create a random
-    // number between two values (-5 and 5), but will exclude a particular value (0) 
-
-    // WRTIE THIS FUNCTION FOR HOMEWORK
-
-    // Next, we will need to figure out why our ball changes speed each time we reload
-    // our game
-
-    // Well, if we think about it, the length of the Vector that we created will ultimately
-    // dictate how much distance that we're going to move each frame
-
-    // This isn't good - we want a Vector that will move the ball in a particular direction,
-    // but at the same speed as every other vector
-
-    // So, how can we do this? 
-    // Well, what do we know about Vectors? They have a length and a direction
-    // 
-    // If we want our ball to move the same distance each frame, then we need to make
-    // sure that every vector that we create has the same length
-    //
-    // To do this, we will need to normalise each Vector the we create
-
-    // What does normalize mean? Well, normalizing a Vector means that we convert it into 
-    // a unit vector (a unit Vector is a Vector which has a length of 1).
-
-    // So, let's create a method that will normalise our vector
     // (See the Vector2 class to see how we wrote the normalize function)
     ballMovementVector.normalize();
 
     // Create a speed for the ball to move
     // This will be used later to speed our movement up
     ballSpeed = 3;
+
+    let paddleWidth = 10;
+    let paddleHeight = 80;
+    let margin = 10;
+
+    leftPaddle = new Rect(
+        paddleWidth,
+        paddleHeight,
+        new Vector2(
+            margin,
+            canvas.clientHeight / 2 - (paddleHeight / 2)
+        )
+    );
 }
 
 function getRandomNumberInRangeExcluding(start, end, excluding) {
@@ -113,12 +79,50 @@ function update() {
 
     ball.position.x += ballMovementVector.x * ballSpeed;
     ball.position.y += ballMovementVector.y * ballSpeed;
+
+    checkCollisions();
 }
+
+function checkCollisions() {
+
+    // Check if the ball is leaving the right hand side of our canvas
+    if (ball.position.x + ball.radius >= canvas.clientWidth) {
+
+        ballMovementVector.x *= -1;
+    }
+    // Check if the ball is leaving the left hand side of our canvas
+    else if (ball.position.x - ball.radius <= 0) {
+
+        ballMovementVector.x *= -1;
+    }
+
+    // Check if the ball is leaving the bottom of our canvas
+    if (ball.position.y + ball.radius >= canvas.clientHeight) {
+
+        ballMovementVector.y *= -1;
+
+        ballColor = altColor1;
+    }
+    // Check if the ball is leaving the top of our canvas
+    else if (ball.position.y - ball.radius <= 0) {
+
+        ballMovementVector.y *= -1;
+        
+        ballColor = altColor2;
+    }
+}
+
+let ballColor = "black";
+let altColor1 = "red";
+let altColor2 = "green";
 
 // Create a function that will re-draw our updated game
 function draw() {
-    clearCanvas("rgb(230, 230, 230)");
-    ball.draw(context, 1, "blue", "green", false);
+    clearCanvas("rgb(230, 230, 230)");  
+    ball.draw(context, 1, ballColor, ballColor, false); // Updated ball draw
+
+    // Draw our left paddle
+    leftPaddle.draw(context, 1, "blue");
 }
 
 function clearCanvas(color) {
@@ -129,3 +133,14 @@ function clearCanvas(color) {
 }
 
 window.addEventListener("load", loadGame);
+
+// Stackoverflow: 
+function getRandomInRangeExcluding(min, max, excl) {
+    let value = excl;
+
+    while (value == excl) {
+        value = Math.floor(Math.random() * (max - min + 1) + min);
+    }
+
+    return value;
+}
