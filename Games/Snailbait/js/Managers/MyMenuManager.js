28f2bcada05e87028db00ffb62892539c2868efa
James Farrell <d00206780@student.dkit.ie>
2021-11-30 08:51:03 +0000

    Add Comments to the MyMenuManager Class

--- Games/Snailbait/js/Managers/MyMenuManager.js
+++ Games/Snailbait/js/Managers/MyMenuManager.js
@@ -11,6 +11,7 @@ class MyMenuManager {
 
     registerForNotifications() {
 
+        // When a 'menu' event fires, call the 'handleMenuNotification' function of 'this' object
         this.notificationCenter.register(
             NotificationType.Menu,
             this,
@@ -32,15 +33,42 @@ class MyMenuManager {
         }
     }
 
+    // If the status type 
     showMenu(statusType) {
+
+        // Check out the initialize function of this class. In it, we create a 'Menu' notification
+        // whenever the play button is pressed. This notification has an action of ShowMenuChanged,
+        // and an argument of [StatusType.Updated | StatusType.Drawn]. The handleMenuNotification 
+        // function of this class is registered to the 'Menu' event. So, it will be called whenever
+        // a 'Menu' notification is created. In the handleMenuNotification, we call this showMenu
+        // function if the notification's action is of type 'ShowMenuChanged'. We also pass through 
+        // the parameters that were added to the notification - [StatusType.Updated | StatusType.Drawn] 
+        // in our case.
+
+        // So, the statusType that is passed to this function will ultimately be [StatusType.Updated |
+        // StatusType.Drawn] (or simply '3', if we work it out). 
+
+        // This means, that when the user presses the 'play' button, a ShowMenuChanged notification is
+        // created, which ultimately tells this function to hide the menu. On the other hand, we could
+        // tell this notification to show the menu, by creating another ShowMenuChanged notification, but
+        // by passing through a StatusType of off.
+
+        // The reason why we use [StatusType.Drawn | StatusType.Updated] to turn off the menu, and 
+        // [StatusType.Off] to turn on the menu, is because the same notification is sent to the
+        // object manager, which ultimately tells it to start Updating and Drawing if the menu is
+        // turned off, or to stop Updating and Drawing if the menu is turned on. Here we see how
+        // one notification may be used to control multiple separate elements.
 
         // If we created an event to tell the ObjectManager to draw and update,
         // then it means we want the game to run i.e. hide the menu
         if (statusType != 0) {
+
             $('#main').hide();
             $('#menu').removeClass('main');
         }
+
         else {
+
             $('#main').show();
             $('#menu').addClass('main');
         }
